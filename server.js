7b29e066578a5b56b46a58b05e9b17813481f32e
Vitor Henrique Cavalcanti <80213150+VitorHenriqueCavalcanti@users.noreply.github.com>
2023-05-18 09:54:30 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,48 +5,81 @@ const {authenticate} = require('@google-cloud/local-auth');
 const {google} = require('googleapis');
 const fs = require('fs').promises;
 const fss = require('fs');
+const path = `caminhodapasta`
 
-
-// Cria o arquivo PDF
 async function generatePDF(rows) {
   const doc = new PDFDocument({size: 'A4'});
-  doc.pipe(fss.createWriteStream('output.pdf'));
   doc.font('Helvetica')
+  doc.image('./logo.png', {width:350,align: 'center'})
   doc.lineGap(15)
-  doc.image('./logo.png', {width:350,align: 'center', lineGap: 10})
   doc.fontSize(16).text('Relatório de ronda noturna', {align: 'center', underline: true});
+  
+  /* DEFININDO AS DATAS PARA BUSCAS */
+  let opcoes = { timeZone: 'UTC', timeZoneName: 'short' };
 
-  let day = 05;
-  let month = '02';
-  let year = 2023;
-  let days = parseInt(day) + 7
+  /* DIA INÍCIO */
+  let dia_inicio = new Date(2023, 4, 07);
+  let mes_inicio = dia_inicio.getMonth();
+  let dataUTC_inicio = dia_inicio.toLocaleDateString(opcoes)
 
-  for(day; day < days; day++){
-    let dateSearch = `${day < 10 ? `${'0'+day}`: day}/${month}/${year}`;
-    console.log(dateSearch)
-    const result = rows.filter(row => row[0] == (dateSearch));
-    result.forEach((row, index) => {
-      doc.moveDown();
-      doc.fontSize(14).text(`Data: ${row[0]}`, {align: 'center'});
-      doc.fontSize(12).text(`Hora de início: ${row[2]}`);
-      doc.fontSize(12).text(`Hora de término: ${row[3]}`);
-      doc.fontSize(12).text(`TA's Margem Direita: ${row[4]}`);
-      doc.fontSize(12).text(`TA's Margem Esquerda: ${row[5]}`);
-      doc.fontSize(12).text(`Inspeção visual dos painéis dos TA's: ${row[6]}`);
-      doc.fontSize(12).text(`Inspeção visual dos painéis das ELF's: ${row[7]}`);
-      doc.fontSize(12).text(`Inspeção de todos elementos nas salas QCM's: ${row[8]}`);
-      doc.fontSize(12).text(`Inspeção de todos elementos nas salas GMG's: ${row[9]}`);
-      doc.fontSize(12).text(`Inspeção de todos elementos nas estruturas dos TA's: ${row[10]}`);
-      doc.fontSize(12).text(`Alguma anomalia e/ou caça-perigo foi encontrado/registrado?: ${row[11].length ? row[11] : 'Não'}`);
-      doc.fontSize(12).text(`Observações: ${doc.moveDown(), row[12]}`, {
-        align: 'justify',
-        width: 500
+  /* DIA FIM */
+  dia_fim = new Date(dia_inicio);
+  dia_fim.setDate(dia_fim.getDate() + 8)
+  let dataUTC_fim = dia_fim.toLocaleDateString(opcoes)
+
+  console.log('RANGE: ', dataUTC_inicio, dataUTC_fim) // DATAS RANGES
+
+
+  /* CHAMADA DAS DATAS EM STRING */
+  function indexReportInicio() {
+   return dataUTC_inicio;
+  }
+  function indexReportFim() {
+    return dataUTC_fim;
+   }
+
+   /* BUSCA DO INDEX */
+
+  let findInicio = rows.findIndex(row => row[0] == indexReportInicio() )
+  console.log('início: ', findInicio) 
+  
+  let findFim = rows.findIndex(row => row[0] == indexReportFim() )
+  console.log('fim: ', findFim)
+  
+  /*
+  PROCURAR O INDEX DO INICIO E CAPTAR ATÉ O INDEX DA DATA FINAL (LIMITE DO ACUMULADOR)
+  */
+
+  for(findInicio; findInicio < findFim; findInicio++){
+    const result = rows.filter((element, index) => index === findInicio);
+    if (result !== undefined) {
+      /* PARA CADA ELEMENTO, INSERE OS VALORES */
+      result.forEach((row, index) => {
+        doc.moveDown();
+        doc.fontSize(14).text(`Data: ${row[0]}`, {align: 'center'});
+        doc.fontSize(12).text(`Hora de início: ${row[2]}`);
+        doc.fontSize(12).text(`Hora de término: ${row[3]}`);
+        doc.fontSize(12).text(`TA's Margem Direita: ${row[4]}`);
+        doc.fontSize(12).text(`TA's Margem Esquerda: ${row[5]}`);
+        doc.fontSize(12).text(`Inspeção visual dos painéis dos TA's: ${row[6]}`);
+        doc.fontSize(12).text(`Inspeção visual dos painéis das ELF's: ${row[7]}`);
+        doc.fontSize(12).text(`Inspeção de todos elementos nas salas QCM's: ${row[8]}`);
+        doc.fontSize(12).text(`Inspeção de todos elementos nas salas GMG's: ${row[9]}`);
+        doc.fontSize(12).text(`Inspeção de todos elementos nas estruturas dos TA's: ${row[10]}`);
+        doc.fontSize(12).text(`Alguma anomalia e/ou caça-perigo foi encontrado/registrado?: ${row[11].length ? row[11] : 'Não'}`);
+        doc.fontSize(12).text(`Observações: ${doc.moveDown(), row[12].length ? row[12] : 'Não houveram fatos relevantes durante as rondas'}`, {
+          align: 'justify',
+          width: 500
+        });
+        doc.fontSize(12).text(`Inspeção visual dos painéis dos TA's: ${row[14]}`);
       });
-      doc.fontSize(12).text(`Inspeção visual dos painéis dos TA's: ${row[14]}`);
-    });
+    } else {
+      console.log('nao encontrado')
+    }
     doc.addPage();
   }
-    doc.end();
+  doc.pipe(fss.createWriteStream(path));
+  doc.end();
 }
 
 
@@ -58,7 +91,7 @@ const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
 const TOKEN_PATH = path.join(process.cwd(), 'token.json');
 const CREDENTIALS_PATH = path.join(process.cwd(), 'client.json');
 
-/**.
+/*
  * @return {Promise<OAuth2Client|null>}
  */
 
@@ -91,7 +124,7 @@ async function saveCredentials(client) {
 }
 
 /**
- * Carrega ou requisita ou autoriza para chamar APIs
+ * Carrega, requisita ou autoriza para chamar APIs
  *
  */
 async function authorize() {
@@ -112,8 +145,8 @@ async function authorize() {
 async function listMajors(auth) {
   const sheets = google.sheets({version: 'v4', auth});
   const res = await sheets.spreadsheets.values.get({
-    spreadsheetId: 'idDaPlanilhas',
-    range: 'Relatório de ronda noturna!B:R',
+    spreadsheetId: 'iddaplanilhagsheet',
+    range: 'nomedaplanilha!B:R',
   });
   const rows = res.data.values;
   if (!rows || rows.length === 0) {
